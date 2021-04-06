25add5ffe4cad3e6427a949b013aa00b61fb0780
nelioyann <nanayannick1998@gmail.com>
2021-04-07 00:20:13 +0200

    added: index to cards

--- src/_data/words.js
+++ src/_data/words.js
@@ -46,5 +46,144 @@ module.exports = {
     {
         jp: "ある",
         en: "be"
+    },{
+        jp: "アット",
+        en: "at"
+    },
+    {
+        jp: "一つ",
+        en: "one"
+    },
+    {
+        jp: "持っている",
+        en: "have"
+    },
+    {
+        jp: "この",
+        en: "this"
+    },
+    {
+        jp: "から",
+        en: "from"
+    },
+    {
+        jp: "バイ",
+        en: "by"
+    },
+    {
+        jp: "ホット",
+        en: "hot"
+    },
+    {
+        jp: "言葉",
+        en: "word"
+    },
+    {
+        jp: "しかし",
+        en: "but"
+    },
+    {
+        jp: "何",
+        en: "what"
+    },
+    {
+        jp: "いくつかの",
+        en: "some"
+    },
+    {
+        jp: "です",
+        en: "is"
+    },
+    {
+        jp: "それ",
+        en: "it"
+    },
+    {
+        jp: "あなた",
+        en: "you"
+    },
+    {
+        jp: "または",
+        en: "or"
+    },
+    {
+        jp: "持っていた",
+        en: "had"
+    },
+    {
+        jp: "インクルード",
+        en: "the"
+    },
+    {
+        jp: "の",
+        en: "of"
+    },
+    {
+        jp: "へ",
+        en: "to"
+    },
+    {
+        jp: "そして",
+        en: "and"
+    },
+    {
+        jp: "は",
+        en: "a"
+    },
+    {
+        jp: "で",
+        en: "in"
+    },
+    {
+        jp: "我々",
+        en: "we"
+    },
+    {
+        jp: "缶",
+        en: "can"
+    },
+    {
+        jp: "アウト",
+        en: "out"
+    },
+    {
+        jp: "その他",
+        en: "other"
+    },
+    {
+        jp: "だった",
+        en: "were"
+    },
+    {
+        jp: "これ",
+        en: "which"
+    },
+    {
+        jp: "やる",
+        en: "do"
+    },
+    {
+        jp: "それらの",
+        en: "their"
+    },
+    {
+        jp: "時間",
+        en: "time"
+    },
+    {
+        jp: "もし",
+        en: "if"
+    },
+    {
+        jp: "意志",
+        en: "will"
+    },
+    {
+        jp: "方法",
+        en: "how"
+    },
+    {
+        jp: "前記",
+        en: "said"
     },]
 }
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -24,27 +24,55 @@ const afficherFeedback = message =>{
 //     utterance.voice = voices[i];
 // }
 // }
-
+let isEnglish = false;
+let isJapanese = false;
 
 let synth = window.speechSynthesis;
 
 
+const getVoices = () => {
+    let voices = synth.getVoices();
+    console.log(voices)
+    for(var i = 0; i < voices.length ; i++) {
+      console.log(voices[i].lang);
+    if(voices[i].lang === "en-US") {
+        // utterance.voice = voices[i];
+        isEnglish = true;
+        // isJapanese = false;
+    } if(voices[i].lang === "ja-JP") {
+      // utterance.voice = voices[i];
+      // isEnglish = false;
+      isJapanese = true;
+    }
+  }
+  return({isEnglish, isJapanese})
+}
 
 
 $$(".card").forEach((card) => {
     card.addEventListener("click", (e) => {
 
+
+
+
+      let isSpeech = getVoices()
+
       let targetWord = e.currentTarget.getAttribute("data-target")
+      let sourceWord = e.currentTarget.getAttribute("data-source")
       console.log(targetWord)
-      let utterance = new SpeechSynthesisUtterance(targetWord);
-      let voices = synth.getVoices();
-      for(var i = 0; i < voices.length ; i++) {
-        console.log(voices[i].lang);
-      if(voices[i].lang === "en-US") {
-          utterance.voice = voices[i];
-      }}
       
-      synth.speak(utterance);
+      if(isSpeech.isJapanese) {
+        let utterance = new SpeechSynthesisUtterance(sourceWord);
+        utterance.lang = "ja-JP";
+        synth.speak(utterance);
+
+      }
+      if(isSpeech.isEnglish) {
+        let utterance = new SpeechSynthesisUtterance(targetWord);
+        utterance.lang = "en-US";
+        synth.speak(utterance);
+
+      }
 
       if ($(".flipped")) {
         // lg()
