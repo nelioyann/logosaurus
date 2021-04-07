9e238de0feff9c8f4c0f6fc6371e6c1ee759a883
nelioyann <nanayannick1998@gmail.com>
2021-04-07 16:25:52 +0200

    fixed: mobile speech synthesis

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,21 +1,12 @@
+document.addEventListener("DOMContentLoaded", ()=>{
 
 window.$ = document.querySelector.bind(document);
 window.$$ = document.querySelectorAll.bind(document);
 
-
-const afficherFeedback = message =>{
-    let feedback = document.querySelector(".feedbackMessage")
-    feedback.innerHTML = message
-    
-
-}
-
-
-
-
-
-
-
+const afficherFeedback = (message) => {
+  let feedback = document.querySelector(".feedbackMessage");
+  feedback.innerHTML = message;
+};
 
 // let voices = speechSynthesis.getVoices();
 // for(var i = 0; i < voices.length ; i++) {
@@ -27,66 +18,75 @@ const afficherFeedback = message =>{
 let isEnglish = false;
 let isJapanese = false;
 
-let synth = window.speechSynthesis;
-
-
+// let synth = window.speechSynthesis;
+let isSpeech = {isEnglish, isJapanese}
 const getVoices = () => {
-    let voices = synth.getVoices();
-    console.log(voices)
-    for(var i = 0; i < voices.length ; i++) {
-      console.log(voices[i].lang);
-    if(voices[i].lang === "en-US") {
-        // utterance.voice = voices[i];
-        isEnglish = true;
-        // isJapanese = false;
-    } if(voices[i].lang === "ja-JP") {
+  if (!speechSynthesis) {
+    alert("API not supported");
+    return { isEnglish, isJapanese };
+  }
+  let voices = speechSynthesis.getVoices();
+  // console.log(voices);
+  for (var i = 0; i < voices.length; i++) {
+    // console.log(voices[i].lang);
+    if (voices[i].lang === "en-US") {
+      // utterance.voice = voices[i];
+      isEnglish = true;
+      // isJapanese = false;
+    }
+    if (voices[i].lang === "ja-JP") {
       // utterance.voice = voices[i];
       // isEnglish = false;
       isJapanese = true;
     }
   }
-  return({isEnglish, isJapanese})
-}
+  console.log("voices retrieved")
+  return { isEnglish, isJapanese };
+};
 
+$("#toggleSpeech").addEventListener("change", (e) => {
+  if (e.currentTarget.checked){
+    console.log("User wants speech")
+    isSpeech = getVoices();
+  } else{
+    console.log("User doesn't want speech")
+
+  }
+})
 
 $$(".card").forEach((card) => {
-    card.addEventListener("click", (e) => {
+  card.addEventListener("click", (e) => {
+    
 
+    let targetWord = e.currentTarget.getAttribute("data-target");
+    let sourceWord = e.currentTarget.getAttribute("data-source");
+    // console.log(targetWord);
 
+    if (isSpeech.isJapanese && $("#toggleSpeech").checked) {
+      let utterance = new SpeechSynthesisUtterance(sourceWord);
+      utterance.lang = "ja-JP";
+      speechSynthesis.speak(utterance);
+    }
+    if (isSpeech.isEnglish && $("#toggleSpeech").checked) {
+      let utterance = new SpeechSynthesisUtterance(targetWord);
+      utterance.lang = "en-US";
+      speechSynthesis.speak(utterance);
+    }
 
-
-      let isSpeech = getVoices()
-
-      let targetWord = e.currentTarget.getAttribute("data-target")
-      let sourceWord = e.currentTarget.getAttribute("data-source")
-      console.log(targetWord)
-      
-      if(isSpeech.isJapanese) {
-        let utterance = new SpeechSynthesisUtterance(sourceWord);
-        utterance.lang = "ja-JP";
-        synth.speak(utterance);
-
-      }
-      if(isSpeech.isEnglish) {
-        let utterance = new SpeechSynthesisUtterance(targetWord);
-        utterance.lang = "en-US";
-        synth.speak(utterance);
-
-      }
-
-      if ($(".flipped")) {
-        // lg()
-        if (e.currentTarget.classList.contains("flipped")) {
-          // clicked book was flipped
-          e.currentTarget.classList.remove("flipped");
-        } else {
-          $(".flipped").classList.remove("flipped");
-          e.currentTarget.classList.add("flipped");
-        }
+    if ($(".flipped")) {
+      // lg()
+      if (e.currentTarget.classList.contains("flipped")) {
+        // clicked book was flipped
+        e.currentTarget.classList.remove("flipped");
       } else {
-        // no cards flipped
+        $(".flipped").classList.remove("flipped");
         e.currentTarget.classList.add("flipped");
       }
-      // lg(e.currentTarget)
-    });
+    } else {
+      // no cards flipped
+      e.currentTarget.classList.add("flipped");
+    }
+    // lg(e.currentTarget)
   });
+});
+})
